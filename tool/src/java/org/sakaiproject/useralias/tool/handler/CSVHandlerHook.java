75257696ba70f840d4a228dbff8f004642d2a2f7
David Horwitz <dhorwitz@gmail.com>
2015-10-31 05:51:45 +0000

    NPJIRA

--- tool/src/java/org/sakaiproject/useralias/tool/handler/CSVHandlerHook.java
+++ tool/src/java/org/sakaiproject/useralias/tool/handler/CSVHandlerHook.java
@@ -21,26 +21,6 @@
 
 package org.sakaiproject.useralias.tool.handler;
 
-/*
- * #%L
- * sakai-useralias-tool
- * %%
- * Copyright (C) 2006 - 2015 Sakai Project
- * %%
- * Licensed under the Educational Community License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- * 
- *             http://opensource.org/licenses/ecl2
- * 
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- * #L%
- */
-
 import java.io.IOException;
 import java.io.OutputStream;
 import java.util.ArrayList;
