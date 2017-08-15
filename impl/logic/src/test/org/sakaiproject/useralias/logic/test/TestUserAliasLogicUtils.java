15c5766a5199ab630ee5ef168e42390a38426dd5
David Horwitz <dhorwitz@gmail.com>
2017-08-15 16:40:47 +0200

    RPLAY-21  test true

--- impl/logic/src/test/org/sakaiproject/useralias/logic/test/TestUserAliasLogicUtils.java
+++ impl/logic/src/test/org/sakaiproject/useralias/logic/test/TestUserAliasLogicUtils.java
@@ -21,77 +21,81 @@
 
 package org.sakaiproject.useralias.logic.test;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
 import org.sakaiproject.memory.api.MemoryService;
 import org.sakaiproject.useralias.dao.impl.UserAliasDao;
 import org.sakaiproject.useralias.logic.impl.UserAliasLogicImpl;
 import org.sakaiproject.useralias.model.UserAliasItem;
 import org.sakaiproject.useralias.model.UserAliasSite;
-import org.sakaiproject.useralias.test.UserAliasTestDataLoad;
-
 import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.junit4.AbstractJUnit4SpringContextTests;
-import org.junit.Assert;
-import org.junit.Before;
-import org.junit.Test;
 
 /**
  * Testing the template processing logic
  * 
  * @author Aaron Zeckoski (aaron@caret.cam.ac.uk)
  */
+@ContextConfiguration(locations = { "/hibernate-test.xml", "classpath:spring-hibernate.xml" })
 public class TestUserAliasLogicUtils extends AbstractJUnit4SpringContextTests {
 
 	private static final String SITE_1_ID = "/site/dgsdgsadgasdf";
- 
+
+	private static Log log = LogFactory.getLog(TestUserAliasLogicUtils.class);
 	protected UserAliasDao dao;
 	protected UserAliasLogicImpl ual;
 	protected MemoryService memService;
-	
-	private UserAliasTestDataLoad userAliasTestDataLoad;
-	protected String[] getConfigLocations() {
-	      // point to the needed spring config files, must be on the classpath
-	      // (add component/src/webapp/WEB-INF to the build path in Eclipse),
-	      // they also need to be referenced in the maven file
-	      return new String[] {"hibernate-test.xml", "spring-hibernate.xml"};
-	   }
 
-	   // run this before each test starts
-	   protected void onSetUpBeforeTransaction() throws Exception {
-	      // load the spring created dao class bean from the Spring Application Context
-	      dao = (UserAliasDao) applicationContext.getBean("org.sakaiproject.useralias.dao.UserAliasDao");
-	      if (dao == null) {
-	         throw new NullPointerException("DAO could not be retrieved from spring context");
-	      }
-	      
-	      ual = new UserAliasLogicImpl();
-	      ual.setDao(dao);
-	      //ual.setCache(memoryService.newCache("org.sakaiproject.useralias.logic.impl.UserAliasLogic.siteCache"));
-	   }
+	// private UserAliasTestDataLoad userAliasTestDataLoad;
 
-   /**
-    * Test method for {@link org.sakaiproject.evaluation.logic.impl.utils.TextTemplateLogicUtils#processTextTemplate(java.lang.String, java.util.Map)}.
-    */
-   @Test
-   public void testUserAliasLogic() {
-	UserAliasSite site1 = new UserAliasSite();
-	site1.setSiteId(SITE_1_ID);
-	dao.save(site1);
-	Assert.assertNotNull(site1.getId());
-	   
-	Assert.assertTrue(ual.realmIsAliased(SITE_1_ID));
-	Assert.assertFalse(ual.realmIsAliased("aasdfasdgasdf"));
-	  
-	//test the name stuff
-	UserAliasItem item1 = new UserAliasItem();
-	item1.setContext(SITE_1_ID);
-	item1.setEid("dDuck");
-	item1.setFirstName("Daffy");
-	item1.setLastName("Duck");
-	item1.setUserId("user1");
-	dao.save(item1);
-	  
-	Assert.assertNotNull(item1.getId());
-	Assert.assertNotNull(ual.getUserAliasItemByIdForContext("user1", SITE_1_ID));
-   }  
+	// run this before each test starts
+	@Before
+	public void onSetUpBeforeTransaction() throws Exception {
+		// load the spring created dao class bean from the Spring Application
+		// Context
+		dao = (UserAliasDao) applicationContext.getBean("org.sakaiproject.useralias.dao.UserAliasDao");
+		if (dao == null) {
+			throw new NullPointerException("DAO could not be retrieved from spring context");
+		}
+
+		ual = new UserAliasLogicImpl();
+		ual.setDao(dao);
+		// ual.setCache(memoryService.newCache("org.sakaiproject.useralias.logic.impl.UserAliasLogic.siteCache"));
+	}
+
+	/**
+	 * Test method for
+	 * {@link org.sakaiproject.evaluation.logic.impl.utils.TextTemplateLogicUtils#processTextTemplate(java.lang.String, java.util.Map)}.
+	 */
+	@Test
+	public void testUserAliasLogic() {
+
+		dao = (UserAliasDao) applicationContext.getBean("org.sakaiproject.useralias.dao.UserAliasDao");
+		if (dao == null) {
+			throw new NullPointerException("DAO could not be retrieved from spring context");
+		}
+		UserAliasSite site1 = new UserAliasSite();
+		site1.setSiteId(SITE_1_ID);
+		log.info("Site: " + site1.toString());
+		dao.save(site1);
+		Assert.assertNotNull(site1.getId());
+
+		Assert.assertTrue(ual.realmIsAliased(SITE_1_ID));
+		Assert.assertFalse(ual.realmIsAliased("aasdfasdgasdf"));
+
+		// test the name stuff
+		UserAliasItem item1 = new UserAliasItem();
+		item1.setContext(SITE_1_ID);
+		item1.setEid("dDuck");
+		item1.setFirstName("Daffy");
+		item1.setLastName("Duck");
+		item1.setUserId("user1");
+		dao.save(item1);
+
+		Assert.assertNotNull(item1.getId());
+		Assert.assertNotNull(ual.getUserAliasItemByIdForContext("user1", SITE_1_ID));
+	}
 }
